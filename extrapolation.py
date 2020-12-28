b72d4d9d339362b6b3934263077b8eaa1a43b5bb
Michał Jaworski <jawor403@gmail.com>
2020-12-28 21:00:53 +0100

    Numerical stability improvements

--- extrapolation.py
+++ extrapolation.py
@@ -2,6 +2,14 @@ import numpy as np
 import torch
 
 
+def normalize(x):
+    s = torch.sum(x)
+    if torch.abs(s) < 1e-10:
+        return torch.ones_like(x)
+    else:
+        return x / s
+
+
 def MPE(X, U, qr=True, objective=None):
     # X: (k, n)
     n, k = U.shape
@@ -14,7 +22,7 @@ def MPE(X, U, qr=True, objective=None):
     else:
         M = A.T @ A
         c[:-1] = torch.solve(A.T @ b, M).solution.flatten()
-    gamma = c / torch.sum(c)
+    gamma = normalize(c)
     return gamma @ X
 
 
@@ -29,7 +37,7 @@ def RRE(X, U, qr=True, objective=None):
         M = U.T @ U
         # M = M / torch.sqrt(torch.sum(M ** 2))
         c = torch.solve(b, M).solution
-    gamma = c / torch.sum(c)
+    gamma = normalize(c)
     return (gamma.T @ X).flatten()
 
 
@@ -40,21 +48,21 @@ def regularized_RRE(X, U, lambda_, objective=None):
     I = torch.eye(k, device=U.device, dtype=U.dtype)
     b = torch.ones((k, 1), device=U.device, dtype=U.dtype)
     c = torch.solve(b, M + lambda_ * I).solution
-    gamma = c / torch.sum(c)
+    gamma = normalize(c)
     return (gamma.T @ X).flatten()
 
 
-def RNA(X, U, objective, lambda_range, linesearch=True, normalize=True):
+def RNA(X, U, objective, lambda_range, linesearch=True, norm=True):
     n, k = U.shape
     solutions = []
     M = U.T @ U
-    if normalize:
+    if norm:
         M = M / torch.linalg.norm(M, 2)
     I = torch.eye(k, device=U.device, dtype=U.dtype)
     b = torch.ones((k, 1), device=U.device, dtype=U.dtype)
     for lambda_ in np.geomspace(lambda_range[0], lambda_range[1], k):
         c = torch.solve(b, M + lambda_ * I).solution
-        gamma = c.T / torch.sum(c)
+        gamma = normalize(c.T)
         solutions.append((gamma @ X).flatten())
     values = [objective(x).item() for x in solutions]
     idx = np.argmin(values)
@@ -88,7 +96,7 @@ def RNA_cholesky(X, U, objective, lambda_range, linesearch=True):
             L[i, i] = d
         y = torch.triangular_solve(b, L, upper=False).solution
         c = torch.triangular_solve(y, L.T, upper=True).solution
-        gamma = c.T / torch.sum(c)
+        gamma = normalize(c.T)
         solutions.append((gamma @ X).flatten())
     values = [objective(x).item() for x in solutions]
     idx = np.argmin(values)
