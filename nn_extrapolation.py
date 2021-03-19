855b2f00d9ae0edf67a4c1337c63ef37f579f6ba
Michał Jaworski <jawor403@gmail.com>
2021-03-19 12:19:17 +0100

    Use groups do allow for different acceleration parameters

--- nn_extrapolation.py
+++ nn_extrapolation.py
@@ -1,58 +1,63 @@
 from collections import deque
 from copy import deepcopy
+from typing import Optional
 
 import torch
 from torch.optim import SGD
+from torch.nn import utils
 
 from extrapolation import difference_matrix, regularized_RRE, RRE
-
-
-def params_to_vector(parameters):
-    param_vectors = []
-    for param in parameters:
-        param_vectors.append(param.data.flatten().cpu())
-    return deepcopy(torch.hstack(param_vectors))
-
-
-def params_from_vector(parameters, x):
-    idx = 0
-    for param in parameters:
-        n = param.data.numel()
-        param.data[:] = x[idx:idx + n].view(param.data.shape)
-        idx += n
 
 
 class AcceleratedSGD(SGD):
 
     def __init__(self, params, lr: float, k: int = 10, lambda_: float = 1e-10, momentum: float = 0,
                  dampening: float = 0, weight_decay: float = 0, nesterov: bool = False,
-                 mode: str = "epoch", method: str = "RNA"):
-        self.k = k
-        self.lambda_ = lambda_
+                 mode: str = "epoch", method: Optional[str] = "RNA"):
         self.mode = mode
-        if method not in ["RNA", "RRE"]:
-            raise RuntimeError("Unknown method: " + method)
-        self.method = method
+        assert method in ["RNA", "RRE", None], "Unknown method: " + method
+        assert k > 0 or method is None, "Acceleration methods require k > 0"
+
         super().__init__(params, lr, momentum, dampening, weight_decay, nesterov)
+        # add custom parameters
+        self.defaults.update(
+            lambda_=lambda_,
+            k=k,
+            method=method,
+        )
+        for group in self.param_groups:
+            group.update(
+                lambda_=lambda_,
+                k=k,
+                method=method,
+            )
+        self.reset_stored_params()
 
     def add_param_group(self, param_group: dict):
         super().add_param_group(param_group)
-        self.reset_stored_params()
+        self._reset_group_params(self.param_groups[-1])
 
-    def reset_stored_params(self):
-        for group in self.param_groups:
-            group["stored_params"] = deque([], maxlen=self.k)
+    def _reset_group_params(self, group):
+        if group.get("method") is not None:
+            group["stored_params"] = deque([], maxlen=group["k"])
             group.pop("accelerated_params", None)
             group.pop("stored_params_avg", None)
 
+    def reset_stored_params(self):
+        for group in self.param_groups:
+            self._reset_group_params(group)
+
     def update_stored_params(self):
         for group in self.param_groups:
-            x = params_to_vector(group["params"])
-            group["stored_params"].append(x)
+            if group.get("method") is not None:
+                x = utils.parameters_to_vector(group["params"]).cpu()
+                group["stored_params"].append(x)
 
     def update_param_avg(self):
         for group in self.param_groups:
-            x = params_to_vector(group["params"])
+            if group.get("method") is None:
+                continue
+            x = utils.parameters_to_vector(group["params"]).cpu()
             if "stored_params_avg" in group:
                 group["stored_params_ctr"] += 1
                 c = 1 / group["stored_params_ctr"]
@@ -63,18 +68,19 @@ class AcceleratedSGD(SGD):
 
     def update_stored_params_from_avg(self):
         for group in self.param_groups:
-            x = group.pop("stored_params_avg", None)
-            group["stored_params"].append(x)
+            if group.get("method") is not None:
+                x = group["stored_params_avg"]
+                group["stored_params"].append(x)
 
     def store_parameters(self, target_groups=None):
         if target_groups is None:
             for group in self.param_groups:
                 if "accelerated_params" in group:
-                    params_from_vector(group["params"], group["accelerated_params"])
+                    utils.vector_to_parameters(group["accelerated_params"], group["params"])
         else:
             for group, target in zip(self.param_groups, target_groups):
                 if "accelerated_params" in group:
-                    params_from_vector(target, group["accelerated_params"])
+                    utils.vector_to_parameters(group["accelerated_params"], target)
 
     def step(self, closure=None):
         super().step(closure)
@@ -91,58 +97,14 @@ class AcceleratedSGD(SGD):
 
     def accelerate(self):
         for group in self.param_groups:
+            if group.get("method") is None:
+                continue
             xs = list(group["stored_params"])
-            if len(xs) < self.k:
+            if len(xs) < group["k"]:
                 raise ValueError("Not enough stored values to accelerate")
             U = difference_matrix(xs)
             X = torch.vstack(xs[1:])
-            if self.method == "RNA":
-                group["accelerated_params"] = regularized_RRE(X, U, self.lambda_)
-            elif self.method == "RRE":
+            if group["method"] == "RNA":
+                group["accelerated_params"] = regularized_RRE(X, U, group["lambda_"])
+            elif group["method"] == "RRE":
                 group["accelerated_params"] = RRE(X, U)
-
-
-class SeparateAcceleratedSGD(AcceleratedSGD):
-
-    def update_stored_params(self):
-        for group in self.param_groups:
-            params = [p.data.flatten().cpu() for p in group["params"]]
-            group["stored_params"].append(params)
-
-    def update_param_avg(self):
-        for group in self.param_groups:
-            params = [p.data.flatten().cpu() for p in group["params"]]
-            if "stored_params_avg" in group:
-                group["stored_params_ctr"] += 1
-                c = 1 / group["stored_params_ctr"]
-                new_avg = []
-                for p, p_avg in zip(params, group["stored_params_avg"]):
-                    new_avg.append(c * p + (1 - c) * p_avg)
-                group["stored_params_avg"] = new_avg
-            else:
-                group["stored_params_avg"] = params
-                group["stored_params_ctr"] = 1
-
-    def store_parameters(self, target_groups=None):
-        if target_groups is None:
-            for group in self.param_groups:
-                if "accelerated_params" in group:
-                    for p, p_acc in zip(group["params"], group["accelerated_params"]):
-                        p.data[:] = p_acc.view(p.data.shape)
-        else:
-            for group, target in zip(self.param_groups, target_groups):
-                if "accelerated_params" in group:
-                    for p, p_acc in zip(target, group["accelerated_params"]):
-                        p.data[:] = p_acc.view(p.data.shape)
-
-    def accelerate(self):
-        for group in self.param_groups:
-            params = list(group["stored_params"])
-            if len(params) < self.k:
-                raise ValueError("Not enough stored values to accelerate")
-            group["accelerated_params"] = []
-            for i in range(len(params[0])):
-                xs = [ps[i] for ps in params]
-                U = difference_matrix(xs)
-                X = torch.vstack(xs[1:])
-                group["accelerated_params"].append(regularized_RRE(X, U, self.lambda_))
